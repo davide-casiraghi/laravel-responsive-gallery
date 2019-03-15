b07dc6c01dbd01bf207871f817920ffd53710a96
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-03-15 15:29:43 +0100

    fixed problem with Bricklayer object, now is a global variable window.Bricklayer

--- resources/assets/js/responsiveGallery.js
+++ resources/assets/js/responsiveGallery.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     
     if( $(".responsiveGallery").length ){
     // You have to create a `Bricklayer` instance for every Bricklayer container you have.
-           var bricklayer = new Bricklayer(document.getElementById('my-bricklayer'));
+           var bricklayer = new window.Bricklayer(document.getElementById('my-bricklayer'));
 
            // You can delete these lines, these adds dynamic bricklayers to make you understand how it works.
            for (var i = 0; i < 20; i++) {
