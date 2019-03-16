ddba0960c093b3dce24726de531a6cbf5a1ce94d
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-03-16 15:40:34 +0100

    now galleries can be fully styled with column-width and gutter data attributes

--- resources/assets/js/responsiveGallery.js
+++ resources/assets/js/responsiveGallery.js
@@ -1,20 +1,9 @@
 
 $(document).ready(function () {
     
-    $(".responsiveGallery").each(function(){
-        // You have to create a `Bricklayer` instance for every Bricklayer container you have.
-        
-        var columnWidth = $(this).attr('data-column-width');
-        var gutter = $(this).attr('data-gutter');
-        
-        $(this).addClass("column-width-"+columnWidth);
-        
-        
-        
+    // You have to create a `Bricklayer` instance for every Bricklayer container you have.
+    $(".responsiveGallery").each(function(){    
         var bricklayer = new window.Bricklayer(this);
-        
     });
     
-    
-
 });
