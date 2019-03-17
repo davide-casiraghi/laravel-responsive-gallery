69d04832587ad88f4304d1f14697085f27e63647
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-03-17 07:03:07 +0100

    added mergeConfigFrom

--- src/ResponsiveGalleryServiceProvider.php
+++ src/ResponsiveGalleryServiceProvider.php
@@ -14,6 +14,10 @@ class ResponsiveGalleryServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/../resources/assets/js' => resource_path('js/vendor/laravel-responsive-gallery/'),
         ], 'js');
+        
+        $this->publishes([
+            __DIR__ . '/../config/responsive-gallery.php' => base_path('config/responsive-gallery.php')
+        ], 'config');
     }
 
     public function register()
