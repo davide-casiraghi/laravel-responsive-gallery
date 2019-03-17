c2b9aa0325f55dca2073501a7e813ab5a59b8cd0
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-03-17 06:59:23 +0100

    added config file

---
+++ config/responsive-gallery.php
@@ -0,0 +1,7 @@
+<?php 
+    return[
+            'table-name' => '',
+            'photo-filename-field' => '',
+            'photo-description-field' => '',
+            'photo-alt-text-field' => '',
+    ];
