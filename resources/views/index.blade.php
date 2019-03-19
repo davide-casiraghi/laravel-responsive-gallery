09f15306de53c73618e0a3189898aed79e306ae6
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-03-19 09:00:30 +0100

    adding the index view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+
+
+Here is the gallery index view!
+:)
+
+{{--
+<blockquote class="quoteOfTheDay">
+    <div class="text">
+        <img class="start-quote" src="/vendor/responsive-quotes/assets/images/start-quote-teal.png" alt="Start Quote">
+        {{--<div class="start-quote">"</div>--}}
+        {{$joke}}
+        <img class="end-quote" src="/vendor/responsive-quotes/assets/images/end-quote-teal.png" alt="End Quote">
+        {{--<div class="end-quote">"</div>--}}
+    </div>
+    <cite>Martha Graham</cite>
+</blockquote>
+--}}
--- src/ResponsiveGalleryServiceProvider.php
+++ src/ResponsiveGalleryServiceProvider.php
@@ -18,6 +18,15 @@ class ResponsiveGalleryServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/../config/responsive-gallery.php' => base_path('config/responsive-gallery.php')
         ], 'config');
+        
+        if (! class_exists('CreateGalleryImagesTable')) {
+            $this->publishes([
+                __DIR__.'/../database/migrations/create_gallery_images_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_gallery_images_table.php'),
+            ], 'migrations');
+        }
+        
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'php-responsive-quote');
+
     }
 
     public function register()
