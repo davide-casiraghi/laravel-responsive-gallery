f013710a518e880cf4a7f7a69c2c830d58a18bac
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-03-21 16:02:11 +0100

    now not using any more partials

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -19,72 +19,63 @@
                 @method('PUT')
                 
                 <div class="row">
+                    
+                    {{-- Image file name --}}
                     <div class="col-12">
                         <div class="form-group file_name">
                             <label for="file_name">Image file name</label>
                             <input type="text" name="file_name" class="form-control{{ $errors->has($galleryImage->file_name) ? ' is-invalid' : '' }}"
                                 @if(!empty($galleryImage->file_name)) value="{{ $galleryImage->file_name }}" @endif
                             >
-                            @if ($errors->has($galleryImage->file_name))
-                                <span class="invalid-feedback" role="alert"><strong>{{ $errors->first($galleryImage->file_name) }}</strong></span>
+                            @if ($errors->has("file_name"))
+                                <span class="invalid-feedback" role="alert"><strong>{{ $errors->first("file_name") }}</strong></span>
                             @endif
                         </div>
-                        
-                        {{--@include('vendor.laravel-responsive-gallery.partials.input', [
-                            'title' => 'Image file name',
-                            'name' => 'file_name',
-                            'placeholder' => '', 
-                            'value' => $galleryImage->file_name
-                        ])--}}
                     </div>
+                    
+                    {{-- Description --}}
                     <div class="col-12">
-                        @include('vendor.laravel-responsive-gallery.partials.textarea-plain', [
-                            'title' =>  'Description',
-                            'name' => 'description',
-                            'value' => $galleryImage->description
-                        ])
+                        <div class="form-group description">
+                            <label for="description">Description</label>
+                            <textarea class="form-control{{ $errors->has("description") ? ' is-invalid' : '' }}" style="height: '9rem'" name="description" id="description" >@if(!empty($galleryImage->description)){!! $galleryImage->description !!} @endif</textarea>
+                            @if ($errors->has("description"))
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first("description") }}</strong>
+                                </span>
+                            @endif
+                        </div>
                     </div>
+                    
+                    {{-- Alt text --}}
                     <div class="col-12">
-                        {{--@include('vendor.laravel-responsive-gallery.partials.input', [
-                            'title' => 'Alt text',
-                            'name' => 'alt',
-                            'placeholder' => '', 
-                            'value' => $galleryImage->alt
-                        ])--}}
                         <div class="form-group alt">
                             <label for="alt">Alt text</label>
                             <input type="text" name="alt" class="form-control{{ $errors->has($galleryImage->alt) ? ' is-invalid' : '' }}"
                                 @if(!empty($galleryImage->alt)) value="{{ $galleryImage->alt }}" @endif
                             >
-                            @if ($errors->has($galleryImage->alt))
-                                <span class="invalid-feedback" role="alert"><strong>{{ $errors->first($galleryImage->alt) }}</strong></span>
+                            @if ($errors->has("alt"))
+                                <span class="invalid-feedback" role="alert"><strong>{{ $errors->first("alt") }}</strong></span>
                             @endif
                         </div>
                     </div>
+                    
+                    {{-- Video Link --}}
                     <div class="col-12">
-                        {{--@include('vendor.laravel-responsive-gallery.partials.input', [
-                            'title' => 'Video link',
-                            'name' => 'video_link',
-                            'placeholder' => '', 
-                            'value' => $galleryImage->video_link
-                        ])--}}
                         <div class="form-group video_link">
                             <label for="video_link">Video link</label>
                             <input type="text" name="video_link" class="form-control{{ $errors->has($galleryImage->video_link) ? ' is-invalid' : '' }}"
                                 @if(!empty($galleryImage->video_link)) value="{{ $galleryImage->video_link }}" @endif
                             >
-                            @if ($errors->has($galleryImage->video_link))
-                                <span class="invalid-feedback" role="alert"><strong>{{ $errors->first($galleryImage->video_link) }}</strong></span>
+                            @if ($errors->has("video_link"))
+                                <span class="invalid-feedback" role="alert"><strong>{{ $errors->first("video_link") }}</strong></span>
                             @endif
                         </div>
-                        
                     </div>
                     
                     
                     <div class="col-12">
-                        @include('vendor.laravel-responsive-gallery.partials.buttons-back-submit', [
-                            'route' => 'responsive-gallery.index'  
-                        ])
+                        <button type="submit" class="btn btn-primary float-right">Submit</button>
+                        <a class="btn btn-outline-primary mr-2 float-right" href="{{ route("responsive-gallery.index") }}">Back</a>
                     </div>
                     
                     
